9ecb9d1c8ede683671fc42ac267f8082819c2d03
kwaegeo <dlehgus113@naver.com>
2022-01-04 04:25:37 +0900

    page

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,15 @@
 package com.cos.blog.test;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +22,56 @@ public class DummyControllerTest {
 
 	@Autowired //의존성 주입
 	private UserRepository userRepository;
+	
+	//ex) http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+		//findAll의 return 타입이 List이기 때문에 한번에 넘어간다.
+	}
+	
+	//한 페이지당 2건에 데이터를 return 받아 볼 예정 
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort="id", direction=Sort.Direction.DESC)Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	
+	//{id}주소로 파라미터를 전달받을 수 있다.
+	//ex) http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		//user/4를 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 되고
+		// 그럼 return null이 되기에 문제가 생긴다.
+		// Optional로 너의 User객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return한다.
+		
+		//타입을 찾고 override핳고 하는 것보다 람다식이 더 편할 수 있다.
+		//익명처리로 해결한다 return 타입을 몰라도된다.
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//			return IllegalArgumentException("해당 사용자는 없습니다.");
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>(){
+			@Override
+			public IllegalArgumentException get() {
+				
+				return new IllegalArgumentException("해당 유저는 없습니다. id" + id);
+			}
+			
+		});
+		//요청 : 웹브라우저
+		//User객체 = 자바오브젝트
+		//변환 (웹브라우저가 이해가능한 데이터로 변환해야함 -> json
+		//스프링 부트는 MessageConverter라는 애가 응답시에 자동 작동
+		//만약에 자바 오브젝트를 return하게 되면 MessageConverter가 Jackson이라는 라이브러리를 호출해서
+		//user 오브젝트를 json으로 변환해서 브라우저에게 던져준다.
+		return user;
+	}
+	
+	
 	
 	//http://localhost:8000/blog/dummy/join(요청)
 	//http의 body에 username, password, email의 데이터를 가지고 요청하게 되면
