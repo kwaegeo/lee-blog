0208ba172a6cacb0ec7c18b180eebc6caf1d6711
kwaegeo <dlehgus113@naver.com>
2022-01-06 02:39:05 +0900

    index

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,14 +3,20 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -22,7 +28,48 @@ public class DummyControllerTest {
 
 	@Autowired //의존성 주입
 	private UserRepository userRepository;
+
+	@DeleteMapping("/dummy/user/{id}")
+	public String deleteUser(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id); //delete는 return하는게 없음(void)
+		} catch(EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다";
+		}
+		
+		
+		
+		return "삭제되었습니다. id " + id;
+	}
 	
+	
+	
+	
+	
+	
+	//save함수는 id를 전달하지 않으면 insert를 해주고 
+	//save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	//save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다.
+	//email, password 2개를 받고  수정 
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json 데이터를 요청 -> Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아준다.)
+		System.out.println("id :"+id);
+		System.out.println("password :"+ requestUser.getPassword());
+		System.out.println("email :" + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});  //이 과정이 영속화 하는 과정 (DB -> 영속성 컨텍스트)
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(user); 
+		
+		//더티 체킹
+		return user;
+	}
 	//ex) http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/users")
 	public List<User> list(){
@@ -58,7 +105,7 @@ public class DummyControllerTest {
 			@Override
 			public IllegalArgumentException get() {
 				
-				return new IllegalArgumentException("해당 유저는 없습니다. id" + id);
+				return new IllegalArgumentException("해당 유저는 없습니다. " );
 			}
 			
 		});
