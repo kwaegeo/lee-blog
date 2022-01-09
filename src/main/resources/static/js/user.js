d1d5f67de8bdb54405bfa631aff3ad46635a2690
kwaegeo <dlehgus113@naver.com>
2022-01-09 23:56:13 +0900

    joinform2

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,23 +1,34 @@
 let index = {
 	init: function(){
-		$("#btn-save").on("click", ()=>{
+		$("#btn-save").on("click", ()=>{ // function(){}, () => {} 이렇게 하는 이유는
+											// this를 바인딩 하기 위해서 사용함.
 			this.save();
 		});
 	
 	},
 	
 	save: function(){
-		//alert("user의 save함수 호출됨");
+		// alert("user의 save함수 호출됨");
 		let data = {
 			username: $("#username").val(),
 			password: $("#password").val(),
 			email: $("#email").val()
 		}
 		
-		//console.log(data);
+		// console.log(data);
 		
-		$.ajax().done().fail(); //ajax통신을 이용해서 3개의 데이터를 json으로 변경해서 insert요청을함!!
+		// ajax호출시 default가 비동기 호출임.
+		 // ajax통신을 이용해서 3개의 데이터를 json으로 변경해서 insert요청을함!!
+		$.ajax({
+			type: "POST",
+			url:"/api/user"
+		}).done(function(){
+		  // 정상이면 done의 함수 실행
+		}).fail(function(){
+		 // 실패면 FAIL의 함수실행
+		});
 	}
 }
 
 index.init();
+
