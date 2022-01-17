a77cb69a8f8783137ac34a05d0865c4fa0f32974
kwaegeo <dlehgus113@naver.com>
2022-01-17 22:50:21 +0900

    updateForm

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,7 +3,15 @@ let index = {
 		$("#btn-save").on("click", ()=>{
 			this.save();
 		});
-
+		
+		$("#btn-delete").on("click", ()=>{
+			this.deletebyId();
+		});
+		
+		$("#btn-update").on("click", ()=>{
+			this.update();
+		});
+		
 	},
 	
 	save: function(){
@@ -27,6 +35,48 @@ let index = {
 		});
 		
 	},
+	
+	
+	deletebyId: function(){
+		let id = $("#id").text();
+		
+		$.ajax({
+			type: "DELETE",
+			url:"/api/board/"+id,
+			dataType: "json"
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+		
+	},
+	
+	update: function(){
+		let id = $("#id").val();
+		
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val(),
+			
+		};
+
+		$.ajax({
+			type: "PUT",
+			url:"/api/board/"+id,
+			data: JSON.stringify(data), 
+			contentType: "application/json; charset=utf-8", 
+			dataType: "json"
+		}).done(function(resp){
+			alert("글수정이 완료되었습니다.");
+			location.href="/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+		
+	},
+		
 }
 index.init();
 
